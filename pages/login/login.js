19f2114de68789621fcd7c21fb3333d8e3a35e49
houpai <houpai@qq.com>
2020-12-31 17:12:51 +0800

    login page complete

--- pages/login/login.js
+++ pages/login/login.js
@@ -7,7 +7,39 @@ Page({
   data: {
     loginBannerBg:loginBg.loginBg,
     passwordType:true,
-    defaultType:true
+    defaultType:true,
+    userName:'',
+    password:''
+  },
+  eyeStatus: function () {
+    this.data.defaultType = !this.data.defaultType
+    this.data.passwordType = !this.data.passwordType
+    this.setData({
+      defaultType: this.data.defaultType,
+      passwordType: this.data.passwordType
+    })
+  },
+  // 账号输入
+  inputWatchUser(e) {
+    this.setData({
+      userName: e.detail.value
+    })
+    console.log('userName ===', this.data.userName)
+  },
+  // 密码输入
+  inputWatchPassword(e) {
+    this.setData({
+      password: e.detail.value
+    })
+    console.log('password ===', this.data.password)
+  },
+  clearUserName() {
+    this.setData({
+      userName: ''
+    })
+  },
+  loginSubmitHandle() {
+    console.log('login btn submit')
   },
   onLoad: function () {
 
