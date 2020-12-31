084bf364db40eb1d9576353230203c22f4d88663
houpai <houpai@qq.com>
2020-12-31 16:31:43 +0800

    first commit

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,11 +1,15 @@
 //index.js
 const app = getApp()
 
+import {loginBg} from "../../assets/base64Img/loginBg"
+
 Page({
   data: {
-   
+    loginBannerBg:loginBg.loginBg,
+    passwordType:true,
+    defaultType:true
   },
   onLoad: function () {
-    
+
   }
 })
