54132e6fac0b41e5569010ecc0d77e61493fa4a3
houpai <houpai@qq.com>
2021-01-02 12:22:08 +0800

    commit

--- pages/openMembership/openMembership.js
+++ pages/openMembership/openMembership.js
@@ -1,5 +1,7 @@
 const app = getApp()
 
+import Dialog from '../../miniprogram_npm/@vant/weapp/dialog/dialog';
+
 Page({
   data: {
     searchValue: '',
@@ -17,6 +19,18 @@ Page({
   // 触底加载 分页逻辑
   scrolltolower(e) {
     console.log("滚动到底部", e);
+  },
+  checkCard(e) {
+    let id = e.currentTarget.dataset['id']
+    console.log('id ===', id)
+    Dialog.confirm({
+      title: '',
+      message: '您现选择价格为180元的“红玫瑰”会员卡,此会员可以提供200积分，你是否开卡？',
+    }).then(() => {
+      // on confirm
+    }).catch(() => {
+      // on cancel
+    });
   },
   onLoad: function () {
 
