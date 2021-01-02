db78969306987cfca832fd3a0443636972b056e8
houpai <houpai@qq.com>
2021-01-02 11:52:00 +0800

    commit

--- pages/customerList/customerList.js
+++ pages/customerList/customerList.js
@@ -2,7 +2,9 @@ const app = getApp()
 
 Page({
   data: {
-    searchValue:''
+    searchValue:'',
+    height: '',
+    top: ""
   },
   onChange(e) {
     this.setData({
@@ -20,5 +22,18 @@ Page({
   },
   onLoad: function () {
     
-  }
+  },
+  onReady: function () {
+    var that = this;
+    wx.createSelectorQuery().select('#topContainer_box').boundingClientRect(function (rect) {
+      that.setData({
+        top: rect.top,
+      })
+    }).exec();
+    setTimeout(() => {
+      that.setData({
+        height: wx.getSystemInfoSync().windowHeight - that.data.top - 100
+      })
+    }, 1000)
+  },
 })
