9b0e2d27fcd95d35da5ba98e06da8a817e9c9006
houpai <houpai@qq.com>
2021-01-10 11:49:46 +0800

    commit

--- pages/dataSys/dataSys.js
+++ pages/dataSys/dataSys.js
@@ -1,6 +1,8 @@
 import * as echarts from '../../components/ec-canvas/echarts';
 import {
-  getCurrentMonthTime
+  getCurrentMonthTime,
+  getCurrentYearTime,
+  formatTime
 } from '../../utils/util'
 
 const app = getApp()
@@ -91,9 +93,17 @@ Page({
     ec: {
       onInit: initChart,
     },
-    startTime: '',
-    endTime: '',
-    timeText: ""
+    startTime: '', // 开始时间
+    endTime: '', // 结束时间
+    timeText: "", // 时间区间文本
+    // 自定义时间区间和文本 优化交互体验 此处单独整 切换类型可保留上次自定义的时间
+    customObj: {
+      startTime: '', // 开始时间
+      endTime: '', // 结束时间
+      timeText: "", // 时间区间文本
+    },
+    calendarShow: false,
+    minDate: 1609430400000, // 自定义时间可选择的最小时间
   },
   // 查看详情 传入页面时间参数
   seeDetail() {
@@ -102,12 +112,30 @@ Page({
       url: `/pages/dataDetail/dataDetail?timeText=${timeText}`
     })
   },
-  // 切换类型
+  // 切换时间类型 默认为索引为type 0 本月 1 本年 2 自定义
   onChange(event) {
-    wx.showToast({
-      title: `切换类型 ${event.detail.name}`,
-      icon: 'none',
-    });
+    this.setData({
+      active:event.detail.name
+    })
+    if (event.detail.name === 0) {
+      let timeObj = getCurrentMonthTime()
+      this.setData({
+        timeText: timeObj.currentMonthTimeText,
+        startTime: timeObj.timeStar,
+        endTime: timeObj.timeEnd
+      })
+    } else if (event.detail.name === 1) {
+      let timeObj = getCurrentYearTime()
+      this.setData({
+        timeText: timeObj.currentMonthTimeText,
+        startTime: timeObj.timeStar,
+        endTime: timeObj.timeEnd
+      })
+    } else if (event.detail.name === 2) {
+      this.setData({
+        calendarShow: true
+      })
+    }
   },
   achievementChange(event) {
     wx.showToast({
@@ -118,6 +146,37 @@ Page({
   // 触底加载 分页逻辑
   scrolltolower(e) {
     console.log("滚动到底部", e);
+  },
+  customTimeSelect() {
+    if (this.data.active !== 2) return false
+    this.setData({
+      calendarShow: true
+    })
+  },
+  // 日历自定义时间区间选择确认
+  timeSelectConfirm(event) {
+    const [start, end] = event.detail;
+
+    let timeStarText = formatTime(new Date(start));
+    timeStarText = timeStarText.slice(0, timeStarText.length - 9)
+
+    let timeEndText = formatTime(new Date(end));
+    timeEndText = timeEndText.slice(0, timeEndText.length - 9)
+
+    this.setData({
+      calendarShow: false,
+      customObj: {
+        startTime: new Date(start).getTime(), // 开始时间
+        endTime: new Date(end + 60 * 60 * 24 * 1000).getTime(), // 结束时间
+        timeText: timeStarText + ' - ' + timeEndText
+      },
+    });
+    console.log('customObj= ===', this.data.customObj)
+  },
+  timeSelectClose() {
+    this.setData({
+      calendarShow: false
+    })
   },
   onLoad: function () {
     let timeObj = getCurrentMonthTime()
--- utils/util.js
+++ utils/util.js
@@ -1,3 +1,12 @@
+const moment = require('./moment.min')
+moment.locale('en', {
+  longDateFormat: {
+    I: 'YYYY-MM_DD',
+    L: 'YYYY-MM-DD HH:mm'
+  }
+})
+
+
 const formatTime = date => {
   const year = date.getFullYear()
   const month = date.getMonth() + 1
@@ -26,26 +35,59 @@ const getCurrentMonthTime = () => {
   let now = new Date(); //当前日期
   let nowMonth = now.getMonth(); //当前月
   let nowYear = now.getFullYear(); //当前年
-  let monthStartDate = new Date(nowYear, nowMonth, 1); 
-  let monthEndDate = new Date(nowYear, nowMonth + 1, 0); 
+  let monthStartDate = new Date(nowYear, nowMonth, 1);
+  let monthEndDate = new Date(nowYear, nowMonth + 1, 0);
   let timeStar = Date.parse(monthStartDate); // 本月的开始时间 ms时间戳 
   let timeEnd = Date.parse(monthEndDate) + 60 * 60 * 24 * 1000; // 本月的结束时间 ms时间戳 为确保精确 取第二天凌晨的时间
 
   let timeStarText = formatTime(new Date(timeStar));
-  timeStarText = timeStarText.slice(0,timeStarText.length - 9)
+  timeStarText = timeStarText.slice(0, timeStarText.length - 9)
 
   let timeEndText = formatTime(new Date(timeEnd));
-  timeEndText = timeEndText.slice(0,timeEndText.length - 9)
+  timeEndText = timeEndText.slice(0, timeEndText.length - 9)
 
 
   return {
-    timeStar:timeStar,
-    timeEnd:timeEnd,
-    currentMonthTimeText:timeStarText + ' - ' + timeEndText
+    timeStar: timeStar,
+    timeEnd: timeEnd,
+    currentMonthTimeText: timeStarText + ' - ' + timeEndText
+  }
+}
+
+
+/**
+ * 获取当年时间区间文本和时间戳
+ * return {
+ *  currentYearTimeText:String,
+ *  timeStar: Number,
+ *  timeEnd:Number
+ * }
+ * */
+
+const getCurrentYearTime = () => {
+
+  //获取本年
+  const startDate = moment().year(moment().year()).startOf('year').valueOf();
+  const endDate = moment().year(moment().year()).endOf('year').valueOf();
+
+  let timeStarText = formatTime(new Date(startDate));
+  timeStarText = timeStarText.slice(0, timeStarText.length - 9)
+
+  let timeEndText = formatTime(new Date(endDate));
+  timeEndText = timeEndText.slice(0, timeEndText.length - 9)
+
+  console.log('startDate====',startDate)
+  console.log('endDate====',endDate)
+
+  return {
+    timeStar: startDate,
+    timeEnd: endDate,
+    currentMonthTimeText: timeStarText + ' - ' + timeEndText
   }
 }
 
 module.exports = {
   formatTime: formatTime,
-  getCurrentMonthTime:getCurrentMonthTime
+  getCurrentMonthTime: getCurrentMonthTime,
+  getCurrentYearTime: getCurrentYearTime
 }
