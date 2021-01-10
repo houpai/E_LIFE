334a969f04d8372e2f3cf746c7777c6862b795eb
houpai <houpai@qq.com>
2021-01-10 21:41:58 +0800

    commit

--- pages/storeSys/storeSys.js
+++ pages/storeSys/storeSys.js
@@ -1,6 +1,23 @@
 //stoeSys.js
 const app = getApp()
 
+// 标注点数据 https://developers.weixin.qq.com/miniprogram/dev/component/map.html
+const INIT_MARKER = {
+	callout: {
+		content: '腾讯总部大楼',
+		padding: 10,
+		borderRadius: 2,
+		display: 'ALWAYS'
+	},
+	latitude: 40.040415,
+	longitude: 116.273511,
+	iconPath: '../../assets/img/Marker1_Activated@3x.png',
+	width: '34px',
+	height: '34px',
+	rotate: 0,
+	alpha: 1
+};
+
 
 Page({
   data: {
@@ -53,8 +70,49 @@ Page({
     firstType:'',
     secondType:"",
     firstShow:true,
-    subActions:[]
+    subActions:[],
+    // 地图相关
+    editAddressShow:false,
+    mapCallbackTxt: '请点击地图标记位置',
+    markers: [{
+			...INIT_MARKER 
+    }], // 店铺标记点
+    scale: 15,
+		location: {
+			latitude: 40.040415,
+			longitude: 116.273511
+		}, // 店铺中心经纬度 和店铺一致的经纬度即可
   },
+  setStoreAddress() {
+    this.setData({
+      editAddressShow:true
+    })
+  },
+  cancelSetAddress() {
+    this.setData({
+      editAddressShow:false
+    })
+  },
+  // 点击地图事件
+	onTapMap (event) {
+		const latitude = event.detail.latitude;
+		const longitude = event.detail.longitude;
+		this.setData({
+      mapCallbackTxt: latitude.toFixed(6) + ',' + longitude.toFixed(6),
+      location: {
+        latitude: latitude,
+        longitude: longitude
+      },
+			markers: [{
+				id: 0, 
+				iconPath: '../../assets/img/Marker1_Activated@3x.png',
+				latitude: latitude,
+        longitude: longitude,
+				width: 30,
+				height: 30
+			}]
+		});
+	},
   storeNameChange(e) {
     this.setData({
       storeName:e.detail
