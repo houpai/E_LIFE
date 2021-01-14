0ebdc622722b982aec4ac4c54cf63bf5c747cb1c
houpai <houpai@qq.com>
2021-01-14 23:15:14 +0800

    commit

---
+++ pages/mIndex/mIndex.js
@@ -0,0 +1,26 @@
+const app = getApp()
+
+Page({
+  data: {
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  onLoad: function () {
+    
+
+  },
+  menuLink(e) {
+    let query = e.currentTarget.dataset['menu']
+    if (query === 'storeIndex') {
+      wx.navigateTo({
+        url: '/pages/storeIndex/storeIndex'
+      })
+    }
+  },
+  goSetting() {
+    // wx.navigateTo({
+    //   url: '/pages/setting/setting'
+    // })
+  }
+})
