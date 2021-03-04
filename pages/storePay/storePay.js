e4cd562bf65966210dd21686db488e8e49238963
houpai <houpai@qq.com>
2021-03-04 22:18:38 +0800

    commit

---
+++ pages/storePay/storePay.js
@@ -0,0 +1,15 @@
+//stoeSys.js
+const app = getApp()
+
+
+Page({
+  data: {
+    storeName:'肯德基首创店',
+    storeType:'餐饮-快餐',
+    storePhone:'13654216382',
+    storeAddress:'辽宁省沈阳市和平区三好街中润国际'
+  },
+  onLoad: function () {
+
+  }
+})
