8cf409f9bb841277a4e9fe4b0a3b0e4da5ff6820
houpai <houpai@qq.com>
2021-03-11 20:36:56 +0800

    commit

--- pages/memberSys/memberSys.js
+++ pages/memberSys/memberSys.js
@@ -2,9 +2,19 @@ const app = getApp()
 
 Page({
   data: {
-    searchValue:'',
+    searchValue: '',
     height: '',
-    top: ""
+    top: "",
+    // 3.10新增
+    selectShow:false,
+    startTimeShow:false,
+    endTimeShow:false,
+    startTime: new Date().getTime(),
+    endTime: new Date().getTime(),
+    startTimeText:'2021-1月',
+    endTimeText:'2021-2月',
+
+    
   },
   onChange(e) {
     this.setData({
@@ -20,8 +30,81 @@ Page({
       url: '/pages/memberDetail/memberDetail'
     })
   },
-  onLoad: function () {
+
+
+  // 3.10新增
+  selectOpen() {
+    this.setData({
+      selectShow:true
+    })
+  },
+
+  // 3.10新增
+  selectShowClose() {
+    this.setData({
+      selectShow:false
+    })
+  },
+
+  // 3.10新增 获取时间戳转换成时间文本
+  submitStartTime() {
+    this.setData({
+      startTimeShow: false
+    });
+  },
+ //  获取时间戳转换成时间文本
+  submitEndTime() {
+    this.setData({
+      endTimeShow: false
+    });
+  },
+  onInputStartTime(event) {
+    this.setData({
+      startTime: event.detail,
+    });
+  },
+  onInputEndTime(event) {
+    this.setData({
+      endTime: event.detail,
+    });
+  },
+  // 确保关闭其他pop
+  selectStartTime() {
+    this.setData({
+      startTimeShow: true,
+      endTimeShow: false
+    });
+  },
+  selectEndTime() {
+    this.setData({
+      endTimeShow: true,
+      startTimeShow: false,
+    });
+  },
+  closeStartTime() {
+    this.setData({
+      startTimeShow: false,
+    });
+  },
+  closeEndTime() {
+    this.setData({
+      endTimeShow: false,
+    });
+  },
+  cancelSelect() {
+    this.setData({
+      selectShow: false,
+    });
+  },
+  submitSelect() {
     
+  },
+
+
+
+
+  onLoad: function () {
+
   },
   onReady: function () {
     var that = this;
