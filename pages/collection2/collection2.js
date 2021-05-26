c08f7eaccae6a77fb5f630c491186115a5a03657
houpai <houpai@qq.com>
2021-05-26 19:09:33 +0800

    .

--- pages/collection2/collection2.js
+++ pages/collection2/collection2.js
@@ -7,6 +7,16 @@ Page({
  
   },
 
+  previewImage(event) {
+    console.log(event.currentTarget.dataset.src)
+    let currentUrl = event.currentTarget.dataset.src
+    // 注：此方法无法预览包内图片 需要改成网络图片
+    wx.previewImage({
+      current: currentUrl, 
+      urls: [currentUrl]
+    })
+  },
+
   onLoad: function () {
 
   },
