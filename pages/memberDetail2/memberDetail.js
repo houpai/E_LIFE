c215587ca61028d6cfe0fe120407ee446fd73803
houpai <houpai@qq.com>
2021-06-02 10:04:50 +0800

    commit

--- pages/memberDetail2/memberDetail.js
+++ pages/memberDetail2/memberDetail.js
@@ -1,12 +1,43 @@
 const app = getApp()
+import {
+  hasValueFromArray
+} from '../../utils/util'
 
 Page({
   data: {
     height: '',
     top: "",
+    cardList:[
+      {
+        id:'15454',
+        type:"类型1",
+        isOpenMore:false,
+        cardItemList:[{id:'2464624r68'},{id:'wdqd4425q6546'}]
+      },
+      {
+        id:'154545443',
+        type:"类型2",
+        isOpenMore:false,
+        cardItemList:[{id:'2464668'},{id:'wdqd5q6546'}]
+      },
+    ], // mock数据 展开收缩isOpenMore
+  },
+  openToggle(e) {
+    let id = e.currentTarget.dataset['id']
+    console.log('id ===', id);
+    let copyCardList = JSON.parse(JSON.stringify(this.data.cardList));
+    let matchingValue = hasValueFromArray(copyCardList,'id',id)
+    console.log('matchingValue ====', matchingValue)
+    if(matchingValue.value) {
+      copyCardList[matchingValue.index].isOpenMore = !copyCardList[matchingValue.index].isOpenMore
+    }
+    this.setData({
+      cardList:copyCardList
+    })
+
   },
   onLoad: function () {
-    
+
   },
   onReady: function () {
     var that = this;
--- utils/util.js
+++ utils/util.js
@@ -37,7 +37,7 @@ const getCurrentMonthTime = () => {
   let nowYear = now.getFullYear(); //当前年
   let monthStartDate = new Date(nowYear, nowMonth, 1);
   let monthEndDate = new Date(nowYear, nowMonth + 1, 0);
-  let timeStar = Date.parse(monthStartDate); // 本月的开始时间 ms时间戳 
+  let timeStar = Date.parse(monthStartDate); // 本月的开始时间 ms时间戳
   let timeEnd = Date.parse(monthEndDate) + 60 * 60 * 24 * 1000; // 本月的结束时间 ms时间戳 为确保精确 取第二天凌晨的时间
 
   let timeStarText = formatTime(new Date(timeStar));
@@ -86,10 +86,47 @@ const getCurrentYearTime = () => {
   }
 }
 
+/**
+ * 判断数组对象中是否存在某个对象
+ * @param arr 数组对象
+ * @param origin(String)要匹配的某个字段
+ * @param value 要匹配的字段值
+ * @return {index: number, value}
+ * */
+const hasValueFromArray = (arr, origin, value) => {
+  if (!Array.isArray) {
+    //Polyfill
+    Array.isArray = function (arg) {
+      return Object.prototype.toString.call(arg) === "[object Array]";
+    };
+  }
+  if (!Array.isArray(arr)) {
+    throw new Error("arr 不是一个数组");
+  } else {
+    let flag = false; //没有匹配
+    let temp = {};
+    let index = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][origin] === value) {
+        flag = true;
+        temp = arr[i];
+        index = i;
+        break;
+      }
+    }
+    if (!flag) {
+      temp = null;
+    }
+    return {index: index, value: temp};
+  }
+}
+
+
+
 
 module.exports = {
   formatTime: formatTime,
   getCurrentMonthTime: getCurrentMonthTime,
   getCurrentYearTime: getCurrentYearTime,
- 
+  hasValueFromArray:hasValueFromArray
 }
